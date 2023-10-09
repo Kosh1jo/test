057f448459fb3a6920d43eb04cbaed143886d161
Kosh1jo <Koshijo34@gmail.com>
2023-10-09 15:26:12 +0700

    Test1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test';
 
 function App() {
   return (
@@ -9,13 +10,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Test/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Learn Reacts test
         </a>
       </header>
     </div>
---
+++ src/Test/test.js
@@ -0,0 +1,5 @@
+function test() {
+    return <div>Test github</div>;
+}
+
+export default test;
